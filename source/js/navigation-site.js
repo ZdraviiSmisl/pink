518de5dd527932afcace40d7f5bba4e1528a0f65
Evgeniy <gormoshka123@gmail.com>
2021-11-15 14:27:55 +0300

    adaptive header of catalog

--- source/js/navigation-site.js
+++ source/js/navigation-site.js
@@ -2,14 +2,14 @@ var pageNav = document.querySelector('.page-navigation');
 var toggleNav = document.querySelector('.page-navigation__toggle');
 
 pageNav.classList.remove('page-navigation--nojs');
-pageNav.classList.add('page-navigation--nav-closed');
+pageNav.classList.add('page-navigation--closed');
 
 toggleNav.addEventListener('click', function() {
-  if (pageNav.classList.contains('page-navigation--nav-closed')) {
-    pageNav.classList.remove('page-navigation--nav-closed');
-    pageNav.classList.add('page-navigation--nav-opened');
+  if (pageNav.classList.contains('page-navigation--closed')) {
+    pageNav.classList.remove('page-navigation--closed');
+    pageNav.classList.add('page-navigation--opened');
   } else {
-    pageNav.classList.add('page-navigation--nav-closed');
-    pageNav.classList.remove('page-navigation--nav-opened');
+    pageNav.classList.add('page-navigation--closed');
+    pageNav.classList.remove('page-navigation--opened');
   }
 });
